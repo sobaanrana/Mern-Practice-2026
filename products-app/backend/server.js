db022539dc38b8a56552f72fabbbb9d6d9b027c2
Sobaan Rana <sobaanrana@gmail.com>
2026-01-16 10:15:24 +0000

    server fixes

--- products-app/backend/server.js
+++ products-app/backend/server.js
@@ -7,13 +7,12 @@ import { connectDB } from "./config/db.js";
 dotenv.config();
 
 const app = express();
-const PORT = 5001;
 
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT, () => {
   connectDB();
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
